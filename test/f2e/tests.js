77a9379baa4f1325d2d799a1520bdf51f7a94dda
Chan-Chun <362732414@qq.com>
2018-08-22 16:07:36 +0800

    adding hookFetch test case

--- test/f2e/tests.js
+++ test/f2e/tests.js
@@ -1,5 +1,8 @@
 describe('test hook', function () {
   it('hookXHR should be ok', function () {
-    chai.assert.strictEqual(window.hookRes, window.normalRes)
-  });
-});
+    chai.assert.deepEqual(window.hookXHRRes, window.normalXHRRes)
+  })
+  it('hookFetch should be ok', function () {
+    chai.assert.deepEqual(window.hookFetchRes, window.normalFetchRes)
+  })
+})
